3bb3b0ba424be0d46cf2050f7c1e07bb4b8720a4
AFvTD <alfreddzek123@gmail.com>
2023-05-05 09:56:30 +0200

    Add action showing windows to buttons

--- Layout/NeuralNetwork/MainWindow.xaml.cs
+++ Layout/NeuralNetwork/MainWindow.xaml.cs
@@ -18,11 +18,51 @@ namespace NeuralNetwork
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CreateNewNeuralNetworkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+            var isWindowClosed = new TaskCompletionSource<object>();
+            var CreateNeuralNetworkWindow = new CreateNeuralNetwork();
+
+            CreateNeuralNetworkWindow.Closed += (s, args) =>
+            {
+                this.Show();
+            };
+
+            CreateNeuralNetworkWindow.Show();
+            var task = Task.Run(async () => await isWindowClosed.Task);
+        }
+
+        private void TrainNeuralNetworkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+            var isWindowClosed = new TaskCompletionSource<object>();
+            var TrainNeuralNetworkWindow = new TrainNeuralNetwork();
+
+            TrainNeuralNetworkWindow.Closed += (s, args) =>
+            {
+                this.Show();
+            };
+
+            TrainNeuralNetworkWindow.Show();
+            var task = Task.Run(async () => await isWindowClosed.Task);
+        }
+
+        private void ExitApplicationButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
