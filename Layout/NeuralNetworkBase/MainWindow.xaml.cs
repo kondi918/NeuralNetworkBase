5f1a1ef903673dc3d1f191338673790fe8238d73
AFvTD <alfreddzek123@gmail.com>
2023-05-06 20:03:44 +0200

    Add Layout Files

---
+++ Layout/NeuralNetworkBase/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeuralNetworkBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateNewNeuralNetworkButton_Click(object sender, RoutedEventArgs e)
+        {
+            var CreateNeuralNetworkWindow = new CreateNeuralNetwork();
+            CreateNeuralNetworkWindow.Show();
+        }
+
+        private void TrainNeuralNetworkButton_Click(object sender, RoutedEventArgs e)
+        {
+            var TrainNeuralNetworkWindow = new TrainNeuralNetwork();
+            TrainNeuralNetworkWindow.Show();
+        }
+
+        private void ExitApplicationButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Layout/NeuralNetworkBase/Neuron.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkBase
+{
+    internal class Neuron
+    {
+        public List<double> weights = new List<double>();       // w0 = bias
+        public List<double> inputData = new List<double>();
+        public double neuronResult { get; set; }
+        public Neuron(double[] weights)
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                this.weights.Add(weights[i]);
+            }
+        }
+        public Neuron(List<double> weights)
+        {
+            for (int i = 0; i < weights.Count; i++)
+            {
+                this.weights.Add(weights[i]);
+            }
+        }
+        public void setInputData(List<double> inputData)
+        {
+            this.inputData.Clear();
+            this.inputData.AddRange(inputData);
+        }
+        public void setInputData(double[] inputData)
+        {
+            this.inputData.Clear();
+            this.inputData.AddRange(inputData);
+        }
+    }
+}
